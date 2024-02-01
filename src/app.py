fbb15896f0fc3f3a09be5646a6fc2e1f5b1c5e3c
Ben Cavins <ben.cavins@flatironschool.com>
2024-02-01 11:20:50 -0500

    add comments

--- src/app.py
+++ src/app.py
@@ -43,10 +43,13 @@ def get_root():
 @app.route('/dogs', methods=['GET', 'POST'])
 def all_dogs():
     if request.method == 'GET':
+        # query for all dogs in db
         all_dogs = Dog.query.all()
+        # serialize all dogs (turn them into a dictionary)
         results = []
         for dog in all_dogs:
             results.append(dog.to_dict())
+        # return the serialized dogs in a response
         return results, 200
     elif request.method == 'POST':
         # get json data from request
@@ -60,23 +63,28 @@ def all_dogs():
         # save new dog in db
         db.session.add(new_dog)
         db.session.commit()
+        # serialize the new dog and return a response
         return new_dog.to_dict(), 201
 
 @app.route('/dogs/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
 def get_dog_by_id(id):
-
+    # query db for the dog with this id
     dog = Dog.query.filter(Dog.id == id).first()
-
+    # if dog does not exist, return a 404
     if dog is None:
         return {'error': 'dog not found'}, 404
-
+    # check which request method was sent
     if request.method == 'GET':
+        # serialize the dog and return a response
         return dog.to_dict(), 200
     elif request.method == 'DELETE':
+        # delete the dog from the db
         db.session.delete(dog)
         db.session.commit()
+        # return a response with and empty body
         return {}, 204
     elif request.method == 'PATCH':
+        # get json data from the request
         json_data = request.get_json()
 
         # method 1
@@ -88,10 +96,13 @@ def get_dog_by_id(id):
         #     dog.breed = json_data.get('breed')
 
         # method 2 (a little better)
+        # loop through the fields in the request json
         for field in json_data:
             # dog.key = json_data.get(key)  # this is what we want to do
+            # set the corresponding attribute on the dog object
             setattr(dog, field, json_data[field])
-        
+        # save the updated dog to the db
         db.session.add(dog)
         db.session.commit()
+        # serialize the dog and return a response
         return dog.to_dict(), 200
